587842278b34d99ca0facba5155b224908f1c71f
Luis Ignacio <luisignacioccp@gmail.com>
2024-06-22 14:10:15 -0400

    chore: Update table name in serverless.yml and handler.js

--- handler.js
+++ handler.js
@@ -1,69 +1,65 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-
 const {
   DynamoDBDocumentClient,
-  GetCommand,
+  QueryCommand,
   PutCommand,
+  ScanCommand,
 } = require("@aws-sdk/lib-dynamodb");
-
 const express = require("express");
 const serverless = require("serverless-http");
 
 const app = express();
 
-const USERS_TABLE = process.env.USERS_TABLE;
+const COTIZATION_TABLE = process.env.COTIZATION_TABLE;
 const client = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(client);
 
 app.use(express.json());
 
-app.get("/users/:userId", async (req, res) => {
+app.get("/cotization/latest", async (req, res) => {
   const params = {
-    TableName: USERS_TABLE,
-    Key: {
-      userId: req.params.userId,
-    },
+    TableName: COTIZATION_TABLE,
+    ScanIndexForward: false, // Orden descendente
+    Limit: 1,
   };
 
   try {
-    const command = new GetCommand(params);
-    const { Item } = await docClient.send(command);
-    if (Item) {
-      const { userId, name } = Item;
-      res.json({ userId, name });
+    const command = new ScanCommand(params);
+    const { Items } = await docClient.send(command);
+    if (Items && Items.length > 0) {
+      const { registration_date, balance } = Items[0];
+      res.json({ registration_date, balance });
     } else {
-      res
-        .status(404)
-        .json({ error: 'Could not find user with provided "userId"' });
+      res.status(404).json({ error: "No cotization records found" });
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: "Could not retrieve user" });
+    res.status(500).json({ error: "Could not retrieve cotization" });
   }
 });
 
-app.post("/users", async (req, res) => {
-  const { userId, name } = req.body;
-  if (typeof userId !== "string") {
-    res.status(400).json({ error: '"userId" must be a string' });
-  } else if (typeof name !== "string") {
-    res.status(400).json({ error: '"name" must be a string' });
-  }
+// app.post("/cotization", async (req, res) => {
+//   const { registration_date, balance } = req.body;
+//   if (typeof registration_date !== "string") {
+//     res.status(400).json({ error: '"registration_date" must be a string' });
+//   } else if (typeof balance !== "number") {
+//     res.status(400).json({ error: '"balance" must be a number' });
+//   }
 
-  const params = {
-    TableName: USERS_TABLE,
-    Item: { userId, name },
-  };
+//   const params = {
+//     TableName: COTIZATION_TABLE,
+//     Item: { registration_date, balance },
+//   };
 
-  try {
-    const command = new PutCommand(params);
-    await docClient.send(command);
-    res.json({ userId, name });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Could not create user" });
-  }
-});
+//   try {
+//     const command = new PutCommand(params);
+//     await docClient.send(command);
+//     res.json({ registration_date, balance });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Could not create cotization" });
+//   }
+// });
 
 app.use((req, res, next) => {
   return res.status(404).json({
